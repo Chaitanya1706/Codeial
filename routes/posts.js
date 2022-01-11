c19581bb0baa2b9610c5db500146ba834551d43d
Chaitanya <17agrawalchaitanya@gmail.com>
2022-01-11 22:50:50 +0530

    added authentication to createPost

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
+const passport = require('passport');
 const router = express.Router();
 const postsController = require('../controllers/posts_controller');
 
 router.get('/view',postsController.viewPost);
 
-router.post('/create',postsController.createPost)
+router.post('/create',passport.checkAuthentication, postsController.createPost)
 
 module.exports = router;
